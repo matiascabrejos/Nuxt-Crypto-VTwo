2a1bbfc282cd924fb6fadb9020dbc11c8f2a764b
matiascabrejos <matias.cabrejos@hotmail.com>
2021-11-10 22:42:36 -0300

    Setting up vuex

---
+++ store/index.js
@@ -0,0 +1,17 @@
+import vuex from "vuex";
+import axios from "axios";
+
+const createStore = () => {
+  return new vuex.Store({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    },
+  });
+};
+
+export default createStore;
