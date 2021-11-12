20764f3d42548ca20e9e880a5e37e8f959f8171a
matiascabrejos <matias.cabrejos@hotmail.com>
2021-11-11 23:11:52 -0300

    Setting up crypto data via vuex and axios

--- pages/CryptoTable.vue
+++ pages/CryptoTable.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>HI !!!!</h1>
+  <div>
+    <div v-for="crypto in cryptos" :key="crypto.id">
+      <h1>{{ crypto.name }}</h1>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    cryptos() {
+      return this.$store.getters.cryptos;
+    },
+  },
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -4,12 +4,33 @@ import axios from "axios";
 const createStore = () => {
   return new vuex.Store({
     state: {
+      cryptos: [],
     },
     mutations: {
+      setCryptos(state, cryptos) {
+        state.cryptos = cryptos;
+      },
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        const url =
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false#";
+        return axios
+          .get(url)
+          .then((res) => {
+            const cryptoData = [];
+            for (const key in res.data) {
+              cryptoData.push({ ...res.data[key] });
+            }
+            vuexContext.commit("setCryptos", cryptoData);
+          })
+          .catch((e) => console.log(e));
+      },
     },
     getters: {
+      cryptos(state) {
+        return state.cryptos;
+      },
     },
   });
 };
