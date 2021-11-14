d27e5c8e899ac9a933a43403e02d008b74cc52de
matiascabrejos <matias.cabrejos@hotmail.com>
2021-11-13 23:06:46 -0300

    Working on the cryptopanel page and the navbar

--- pages/CryptoPanel.vue
+++ pages/CryptoPanel.vue
@@ -1,3 +1,76 @@
 <template>
-    <h1>HI!</h1>
+  <div
+    class="flex items-center w-screen min-h-screen bg-gray-900"
+    style="font-family: 'Muli', sans-serif"
+  >
+    <div class="container ml-auto mr-auto flex flex-wrap items-start">
+      <div class="w-full pl-5 lg:pl-2 mb-4 mt-4"></div>
+      <div
+        class="w-full md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2"
+        v-for="crypto in cryptos"
+        :key="crypto.id"
+      >
+        <div
+          class="
+            rounded-lg
+            m-h-64
+            p-2
+            transform
+            hover:translate-y-2 hover:shadow-xl
+            transition
+            duration-300
+          "
+        >
+          <div class="rounded-lg p-4 bg-indigo-500 flex flex-col">
+            <div>
+              <h5
+                class="text-right"
+                v-bind:style="{
+                  color: crypto.price_change_percentage_24h > 0 ? 'green' : 'red',
+                }"
+              >
+                {{ crypto.price_change_percentage_24h.toFixed(2) }}%
+              </h5>
+              <h5 class="text-white text-2xl font-bold leading-none">
+                {{ crypto.name }}
+              </h5>
+              <span class="text-xs text-gray-400 leading-none">{{
+                crypto.symbol.toUpperCase()
+              }}</span>
+            </div>
+            <div class="flex items-center">
+              <div class="text-lg text-white font-light">
+                {{ crypto.current_price }} US$
+              </div>
+              <img
+                class="
+                  rounded-full
+                  focus:outline-none
+                  w-10
+                  h-10
+                  flex
+                  ml-auto
+                  transition
+                  duration-300
+                  stroke-current
+                  ring ring-accent ring-offset-base-100 ring-offset-2
+                "
+                v-bind:src="crypto.image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    cryptos() {
+      return this.$store.getters.cryptos;
+    },
+  },
+};
+</script>
--- pages/CryptoTable.vue
+++ pages/CryptoTable.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="overflow-x-auto container mx-auto px-4">
-    <div class="form-control pb-6">
-      <label class="label">
-        <span class="label-text pt-2 pb-2 m-auto">Name/Symbol Filter</span>
-      </label>
-      <input
-        type="text"
-        placeholder="Name/Symbol"
-        class="input input-accent input-bordered text-center"
-        v-model="textSearch"
-        @keyup="cryptoFilter()"
-      />
+  <div class="bg-gray-900">
+    <div class="overflow-x-auto container mx-auto px-4">
+      <div class="form-control pb-6">
+        <label class="label">
+          <span class="label-text pt-2 pb-2 m-auto">Name/Symbol Filter</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Name/Symbol"
+          class="input input-accent input-bordered text-center"
+          v-model="textSearch"
+          @keyup="cryptoFilter()"
+        />
+      </div>
+      <table class="table w-full table-compact table-zebra mb-6">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>NAME</th>
+            <th>PRICE</th>
+            <th>CHANGE 24H</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="crypto in filteredCryptos" :key="crypto.id">
+            <th><img class="w-10 rounded-full" v-bind:src="crypto.image" /></th>
+            <td>{{ crypto.name }} ( {{ crypto.symbol.toUpperCase() }} )</td>
+            <td>{{ crypto.current_price }} US$</td>
+            <td
+              v-bind:style="{
+                color: crypto.price_change_percentage_24h > 0 ? 'green' : 'red',
+              }"
+            >
+              {{ crypto.price_change_percentage_24h.toFixed(2) }}%
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <table class="table w-full table-compact table-zebra mb-6">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>NAME</th>
-          <th>PRICE</th>
-          <th>CHANGE 24H</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="crypto in filteredCryptos" :key="crypto.id">
-          <th><img class="w-10 rounded-full" v-bind:src="crypto.image" /></th>
-          <td>{{ crypto.name }} ( {{ crypto.symbol.toUpperCase() }} )</td>
-          <td>{{ crypto.current_price }} US$</td>
-          <td
-            v-bind:style="{
-              color: crypto.price_change_percentage_24h > 0 ? 'green' : 'red',
-            }"
-          >
-            {{ crypto.price_change_percentage_24h.toFixed(2) }}%
-          </td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
