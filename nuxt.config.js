3ce1dea694b9c564b90ba9aab38781b06ba53002
matiascabrejos <matias.cabrejos@hotmail.com>
2022-07-05 21:33:27 -0300

    Some configs to deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 export default {
+  mode: "universal",
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "nuxt-crypto-app",
--- store/index.js
+++ store/index.js
@@ -14,7 +14,7 @@ const createStore = () => {
     actions: {
       nuxtServerInit(vuexContext, context) {
         const url =
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false#";
+          process.env.API_KEY;
         return axios
           .get(url)
           .then((res) => {
